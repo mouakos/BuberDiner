d08f67c96a0718019c0022dd88a2a6f9368ba20a
Stephane Mouako <stephane.mouako@sioux.eu>
2024-10-29 12:19:17 +0100

    Added problem details for global error handling

--- BuberDiner.Api/Filters/ErrorHandlingFilterAttribute.cs
+++ BuberDiner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BuberDiner.Api.Filters;
 
@@ -8,10 +10,15 @@ public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
     public override void OnException(ExceptionContext context)
     {
         var exception = context.Exception;
-        context.Result = new ObjectResult(new { error = "An error occured while processing request." })
+
+        var problemDetails = new ProblemDetails
         {
-            StatusCode = 500
+            Type = "http://tools.ietf.org/tml/rfc7231#section-6.6.1",
+            Title = "An error occured while processing request.",
+            Status = (int)HttpStatusCode.InternalServerError,
         };
+
+        context.Result = new ObjectResult(problemDetails);
         context.ExceptionHandled = true;
     }
 }
